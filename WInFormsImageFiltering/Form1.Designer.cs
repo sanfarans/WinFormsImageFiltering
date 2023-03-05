b6e823c74a1a688146efe2b127f1d91cd8622cf8
sanfarans <sanfarans@gmail.com>
2023-03-05 19:35:57 +0100

    Add basic layout

--- WInFormsImageFiltering/Form1.Designer.cs
+++ WInFormsImageFiltering/Form1.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.loadImageButton = new System.Windows.Forms.Button();
+            this.outputImage = new System.Windows.Forms.PictureBox();
+            this.inputImage = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.saveImageButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.outputImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputImage)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Enabled = false;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.saveImageButton);
+            this.splitContainer1.Panel1.Controls.Add(this.loadImageButton);
+            this.splitContainer1.Panel1.Controls.Add(this.outputImage);
+            this.splitContainer1.Panel1.Controls.Add(this.inputImage);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1182, 653);
+            this.splitContainer1.SplitterDistance = 429;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // loadImageButton
+            // 
+            this.loadImageButton.Location = new System.Drawing.Point(506, 40);
+            this.loadImageButton.Name = "loadImageButton";
+            this.loadImageButton.Size = new System.Drawing.Size(170, 29);
+            this.loadImageButton.TabIndex = 2;
+            this.loadImageButton.Text = "Load Image";
+            this.loadImageButton.UseVisualStyleBackColor = true;
+            // 
+            // outputImage
+            // 
+            this.outputImage.Dock = System.Windows.Forms.DockStyle.Right;
+            this.outputImage.Location = new System.Drawing.Point(682, 0);
+            this.outputImage.Name = "outputImage";
+            this.outputImage.Size = new System.Drawing.Size(500, 429);
+            this.outputImage.TabIndex = 1;
+            this.outputImage.TabStop = false;
+            // 
+            // inputImage
+            // 
+            this.inputImage.Dock = System.Windows.Forms.DockStyle.Left;
+            this.inputImage.Location = new System.Drawing.Point(0, 0);
+            this.inputImage.Name = "inputImage";
+            this.inputImage.Size = new System.Drawing.Size(500, 429);
+            this.inputImage.TabIndex = 0;
+            this.inputImage.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1182, 220);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // saveImageButton
+            // 
+            this.saveImageButton.Location = new System.Drawing.Point(506, 75);
+            this.saveImageButton.Name = "saveImageButton";
+            this.saveImageButton.Size = new System.Drawing.Size(170, 29);
+            this.saveImageButton.TabIndex = 4;
+            this.saveImageButton.Text = "Save Image";
+            this.saveImageButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(10, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 90);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Inversion";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.outputImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputImage)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.PictureBox outputImage;
+        private System.Windows.Forms.PictureBox inputImage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button saveImageButton;
+        private System.Windows.Forms.Button button1;
     }
 }
